e946c7851fdcd3552643a479842e4a938765e3b4
evcgill <130026473+evcgill@users.noreply.github.com>
2023-04-26 20:45:15 -0700

    lab5
    lab5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,10 +1,23 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Data Types & Variables
+// Author: Evelyn Gill <evcgill@ucsc.edu>
+// Date: 04/26/2023
 
 // Constants
+make = "Toyota";
+model = "RAV4";
+color = "White";
+year = 2006;
+ownIt = true;
 
-// Functions
+// Functions & Calculate
+age = 2023 - year;
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + make + " years<br>");
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
