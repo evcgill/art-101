feb1912000fa0445fd3068c8453bf8eee70b8658
evcgill <130026473+evcgill@users.noreply.github.com>
2023-05-02 15:59:12 -0700

    lab73
    lab73

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -1,38 +1,29 @@
-// index.js - Lab 6
+// index.js - Lab 7
 // Author: Evelyn (Evie) Gill
-// Date: 5/1/23
+// Date: 5/2/23
 
 // Constants
-myTransport = ["Toyota RAV4", "Beach Bike"];
-
-myMainRide = {
-  make: "Toyota",
-  model: "RAV4",
-  color: "Blizzard Pearl",
-  year: 2006,
-
-  age : function(){
-    return 2023 - this.year;
-  }
-}
-
-document.writeln("How I Get To Places: ", myTransport, "<br>");
-document.writeln("My Main Ride: <pre>",
-  JSON.stringify(myMainRide, null, '\t'), "</pre>");
-
 
 // Functions
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+function sortUserName() {
+  var userName = window.prompt("Please enter your name.");
+  console.log("userName =", userName)
+  userName = userName.toLowerCase();
+
+  var nameArray = userName.split('');
+  console.log("nameArray =", nameArray);
+
+  var nameArraySort = nameArray.sort();
+  console.log("nameArraySort =", nameArraySort);
+
+  var nameSorted = nameArraySort.join('');
+  console.log("nameSorted =", nameSorted);
+
+  return nameSorted;
+
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+document.writeln("It is fixed: ",
+sortUserName(), "</br>");
