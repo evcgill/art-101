1325fd4646ae2a42b911e1080021898425acf95a
evcgill <130026473+evcgill@users.noreply.github.com>
2023-05-10 21:01:04 -0700

    lab9
    lab9

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -11,14 +11,19 @@
  //creates new elements
  var new1El = document.createElement("p");
  var new2El = document.createElement("p");
+ var new3El = document.createElement("p");
 
  //makes test for the elements
  new1El.innerHTML="Panguins are funny";
  new2El.innerHTML="Live, Laugh, Ahahahaha";
+ new3El.innerHTML="Scooby Dooby Doo";
 
  //tells the elements to go to the HTML
  outputEl.appendChild(new1El);
  outputEl.appendChild(new2El);
+ outputEl.appendChild(new3El);
+ outputEl.insertBefore(new3El, new1El);
+
 
  //changes the color of the text
  new1El.style.color="purple";
