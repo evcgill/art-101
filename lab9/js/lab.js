88a34f7bb7447b64f850f2ca5bc7f1381cd17b66
evcgill <130026473+evcgill@users.noreply.github.com>
2023-05-10 20:28:01 -0700

    l9
    l9

---
+++ lab9/js/lab.js
@@ -0,0 +1,25 @@
+// index.js - Lab 8
+// Author: Evelyn (Evie) Gill
+// Date: 5/10/23
+
+// Constants
+
+// Functions
+//makes an ID for output
+ var outputEl = document.getElementById("output");
+
+ //creates new elements
+ var new1El = document.createElement("p");
+ var new2El = document.createElement("p");
+
+ //makes test for the elements
+ new1El.innerHTML="Panguins are funny";
+ new2El.innerHTML="Live, Laugh, Ahahahaha";
+
+ //tells the elements to go to the HTML
+ outputEl.appendChild(new1El);
+ outputEl.appendChild(new2El);
+
+ //changes the color of the text
+ new1El.style.color="purple";
+ new2El.style.color="red";
