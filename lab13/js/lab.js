2dc578ebfac21b240e186667b33bbd004c375b4c
evcgill <130026473+evcgill@users.noreply.github.com>
2023-05-24 21:41:06 -0700

    lab13
    lab 13

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -5,44 +5,13 @@
 // Constants
 
 // Functions
-// var max = $("#input").val();
-// var oneLongString = "";
-
-// function fillBuzzBoom(maxNums){
-// //loop that lists the numbers and checks if they are fizz buzz boom through division
-//  for (i=1; i <=200; i++) {
-//   if (i%105 == 0) {
-//     oneLongString += i + " - FizzBuzzBoom!<br>";
-//     } else if (i%35 == 0) {
-//       oneLongString += i + " - BuzzBoom!<br>";
-//     } else if (i%21 == 0) {
-//       oneLongString += i + " - FizzBoom!<br>";
-//     } else if (i%15 == 0) {
-//       oneLongString += i + " - FizzBuzz!<br>";
-//     } else if (i%7 == 0) {
-//       oneLongString += i + " - Boom!<br>";
-//     } else if (i%5 == 0) {
-//       oneLongString += i + " - Buzz!<br>";
-//     } else if (i%3 == 0) {
-//       oneLongString += i + " - Fizz!<br>"; 
-//     } else {
-//       oneLongString += i + "<br>";
-//     }   
-//  }
-//  return oneLongString
-// }
-
-// $("#button").click(function(){
-//   $("output").html(fizzBuzzBoom(max));
-// });
-
 var oneLongString = "";
 
-function fizzBuzzBoom(){
+function fizzBuzzBoom(maxNums){
 
-  for (i=1; i<=200; i++) {
+  for (i=1; i<=maxNums; i++) {
 
-// loop that lists the numbers and checks if they are fizz buzz boom through division
+// //loop that lists the numbers and checks if they are fizz buzz boom through division
     if (i%105 == 0) {
       oneLongString += i + " - FizzBuzzBoom!<br>";
     }else if (i%35== 0) {
@@ -61,9 +30,14 @@ function fizzBuzzBoom(){
       oneLongString += i + "<br>";
     }
   }
-  return oneLongString;
+
+  var styledOutput = '<span style="color: pink;">' + oneLongString + '</span>';
+  return styledOutput;
 
 }
 
 
-$("#output").append(fizzBuzzBoom());
+$("#button").click(function(){
+  var max = $("#input").val();
+  $("#output").append(fizzBuzzBoom(max));
+});
